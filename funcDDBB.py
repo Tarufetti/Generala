fe305a6909649bbf774f73bff4e6f525202eb698
Tarufetti <agn_gnr@hotmail.com>
2023-03-24 17:31:37 -0300

    agregada iteracion sobre dict jugadores

--- funcDDBB.py
+++ funcDDBB.py
@@ -1,4 +1,10 @@
 import mysql.connector
+import json
 
 base = mysql.connector.connect('graladice.sql')
 c = base.cursor()
+
+def guardar_partida(JUGADORES:dict, numero_partida:int) -> None:
+    for _, jugador in JUGADORES.items():
+        puntaje = json.dumps(jugador.puntaje)
+        jug = (jugador.nombre, puntaje)
