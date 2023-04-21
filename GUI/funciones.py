0deca280fed6cdc7fb84a12d4359a09b6d1d9638
Tarufetti <agn_gnr@hotmail.com>
2023-04-21 17:24:12 -0300

    creado boton puntajes

---
+++ GUI/funciones.py
@@ -0,0 +1,85 @@
+import customtkinter as ctk
+
+
+def nueva_partida():
+    '''
+    Comienza nueva partida.
+    '''
+    cant_jugadores = input('\nSeleccione la cantidad de jugadores: ')
+    while not cant_jugadores.isdigit() or int(cant_jugadores) > 10:
+        print('\nERROR! Lo ingresado no fue recibido correctamente.\nPor favor, ingrese una opción válida usando NÚMEROS.')
+        cant_jugadores = input('\nSeleccione la cantidad de jugadores: ')
+    JUGADORES = defaultdict(lambda: 'No existe dicho jugador')
+    numero_partida = 1#recolectar de BD el numero, puse 1 para probar
+    for i in range(1, int(cant_jugadores)+1):
+        x = input(f'\nElige el nombre del jugador {i}: ')
+        JUGADORES[i] = Jugador(x,numero_partida)
+    for turno in range(1,12):
+        print(f'\n*** Ronda numero {turno} ***\n')
+        for numero, jugador in JUGADORES.items():
+            print(f'\nEs el turno del jugador #{numero}: {jugador.nombre}')
+            dados_elegidos = tirada([])
+            menu_despues_de_tirada(dados_elegidos,jugador.puntaje[1],jugador)
+            jugador.puntaje[0] += 1
+        if pregunta_continuar(numero_partida):
+            pass
+    sumar_puntajes(JUGADORES)
+    volver_a_jugar = input(f'\nDesea volver a jugar?\nPresione 1 para volver a jugar o ENTER para finalizar\n')
+    if volver_a_jugar == '1':
+        nueva_partida()
+    else:
+        cerrar_partida()
+
+def reanudar_partida(numero_partida:int):
+    '''
+    Toma como parametro la ultima partida y la retoma de donde quedó
+    '''
+    llamado_BD = 'jugadores de la BD de esa partida' #Iterar para crear instancias de la clase jugador
+    JUGADORES = defaultdict(lambda: 'No existe dicho jugador')
+    for _,jugador in JUGADORES.items():
+        print(f'\nJugador #{_}: {jugador.nombre}\nPuntaje parcial:\n')
+        headers = ['# Turno', '# Tiro', 'Escalera', 'Full', 'Poker', 'Generala', 'Generala doble', '1', '2', '3', '4', '5', '6']
+        print(tabulate(jugador.puntaje[:-1], headers=headers, tablefmt="rst"))
+    ronda = JUGADORES[1].puntaje[0]
+    for turno in range(ronda,12):
+        print(f'\n*** Ronda numero {turno} ***\n')
+        for numero, jugador in JUGADORES.items():
+            print(f'\nEs el turno del jugador #{numero}: {jugador.nombre}')
+            dados_elegidos = tirada([])
+            menu_despues_de_tirada(dados_elegidos,jugador.puntaje[1],jugador)
+            jugador.puntaje[0] += 1
+            if pregunta_continuar(numero_partida):
+                pass
+    sumar_puntajes(JUGADORES)
+    volver_a_jugar = input(f'\nDesea volver a jugar?\nPresione 1 para volver a jugar o ENTER para finalizar\n')
+    if volver_a_jugar == '1':
+        nueva_partida()
+    else:
+        cerrar_partida()
+
+def puntajes_altos(root,bienvenido, boton_n_partida, boton_r_partida, boton_puntajes_altos):
+    '''
+    Muestra los 10 puntajes mas altos conseguidos en el juego
+    '''
+    #llamar a la BBDD y conseguir los 10 puntajes mas altos
+    string_puntajes_altos = f'1 - 250  PEPE\n2 - 240  CARLOS\n3 = 230  JUAN\n4 = 220  JUAN\n5 = 210  JUAN\n6 = 200  JUAN\n7 = 190  JUAN\n8 = 180  JUAN\n9 = 170  JUAN\n10 = 150  JUAN'
+    
+    bienvenido.place_forget()
+    boton_n_partida.place_forget()
+    boton_r_partida.place_forget()
+    boton_puntajes_altos.place_forget()
+    header_puntajes = ctk.CTkLabel(root, text='PUNTAJES MAS ALTOS', font=('roboto',60,'bold'))
+    header_puntajes.place(relx= 0.25, rely=0.12)
+    puntajes_altos_label = ctk.CTkLabel(master=root, width=300,text=string_puntajes_altos, font=('roboto',24),justify='left')
+    puntajes_altos_label.place(relx= 0.45, rely=0.30)
+    boton_vuelta = ctk.CTkButton(master=root, width=100, text='Volver',fg_color='blue',font=('roboto',14), command=lambda: boton_volver(bienvenido, puntajes_altos_label, header_puntajes, boton_vuelta, boton_n_partida, boton_r_partida, boton_puntajes_altos))
+    boton_vuelta.place(relx= 0.20, rely=0.05)
+
+def boton_volver(bienvenido, puntajes_altos_label, header_puntajes, boton_vuelta, boton_n_partida, boton_r_partida, boton_puntajes_altos):
+    puntajes_altos_label.place_forget()
+    header_puntajes.place_forget()
+    boton_vuelta.place_forget()
+    bienvenido.place(relx= 0.20, rely=0.10)
+    boton_n_partida.place(relx= 0.48, rely=0.30)
+    boton_r_partida.place(relx= 0.48, rely=0.45)
+    boton_puntajes_altos.place(relx= 0.48, rely=0.60)
--- GUI/generala.py
+++ GUI/generala.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 import CTkMessagebox
 from PIL import Image
 import os
+import funciones
 
 #Creacion del cuadro principal de la GUI
 root = ctk.CTk()
@@ -25,28 +26,45 @@ frame_izq.grid(row=0, column=0, rowspan=4, sticky="nsew")
 #Creacion del cuadro de entrada y boton submit principal
 entry = ctk.CTkEntry(root)
 entry.grid(row=3, column=1, columnspan=6, padx=(20, 0), pady=(20, 20), sticky="nsew")
-boton_submit = ctk.CTkButton(master=root,text='Tirar dados!', fg_color="red")
+boton_submit = ctk.CTkButton(master=root,text='', fg_color="grey", state='dissabled')
 boton_submit.grid(row=3, column=7, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+#Consulta a la BBDD por el numero de la ultima partida
+numero_partida = 1
 
 #Creacion de las imagenes
 dice = []
-dice_relx = 0.20
+dice_relx = 0.20 # incrementa de a 0.15 para mantener simetria
+dice_rely = 0.15
 for i in range(1,6):
     img = ctk.CTkImage(Image.open(f'{cwd}/resources/dice{i}.png'),size=(100,100))
     img_label = ctk.CTkLabel(master=root,text='', image=img)
-    img_label.place(relx=dice_relx, rely = 0.15)
-    dice_relx += 0.15
     dice.append(img_label)
-
 
 #Creacion de los checkboxes
 checkboxes = []
-check_relx = 0.25
+check_relx = 0.25 #incrementa de a 0.15
+check_rely = 0.45
 for i in range(1,6):
     checkbox = ctk.CTkCheckBox(root, text=f'{i}')
-    checkbox.place(relx= check_relx, rely= 0.5)
-    check_relx += 0.15
     checkboxes.append(checkbox)
 
+bienvenido = ctk.CTkLabel(root, text='Bienvenido a la Generala!', font=('roboto',60, 'bold'))
+bienvenido.place(relx= 0.20, rely=0.10)
 
-root.mainloop()
+boton_n_partida = ctk.CTkButton(root, width=200, height=60, text='Nueva Partida', font=('roboto', 20), fg_color='blue', command=lambda: funciones.nueva_partida())
+boton_n_partida.place(relx= 0.48, rely=0.30)
+boton_r_partida = ctk.CTkButton(root, width=200, height=60, text='Reanudar Partida', font=('roboto', 20), fg_color='blue', command=lambda: funciones.reanudar_partida(numero_partida))
+boton_r_partida.place(relx= 0.48, rely=0.45)
+boton_puntajes_altos = ctk.CTkButton(root, width=200, height=60, text='Puntajes mas altos', font=('roboto', 20), fg_color='blue',command=lambda: funciones.puntajes_altos(root,bienvenido, boton_n_partida, boton_r_partida, boton_puntajes_altos))
+boton_puntajes_altos.place(relx= 0.48, rely=0.60)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    root.mainloop()
